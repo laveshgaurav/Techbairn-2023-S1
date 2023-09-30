194a0aa477e46a5cf87e3866ed1ecb97de323f1e
Lavesh Gaurav <39463503+laveshgaurav@users.noreply.github.com>
2023-09-30 20:48:52 +0530

    add blog

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,43 @@
 import { useEffect, useState } from "react";
-import { getAllBlogs } from "../services/http.service";
+import { getAllBlogs, saveBlog } from "../services/http.service";
 import "./Home.css";
 import moment from "moment/moment";
 
 function Home() {
   const [blogData, setBlogData] = useState([]);
+  const [body, setBody] = useState({
+    userId: "64eb651431a0030ecfa1cfb1",
+    title: "",
+    content: "",
+    image: [],
+    location: "India",
+  });
+  const [activeOption, setActiveOption] = useState("link");
+  const [imgUrl, setImgUrl] = useState("");
+
   const fetchData = async () => {
     try {
       let apiData = await getAllBlogs();
       console.log("API DATA", apiData);
       if (apiData?.status) setBlogData(apiData.blogs);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const savePost = async () => {
+    try {
+      let resp = await saveBlog(body);
+      if (resp.status === true) {
+        fetchData();
+        setBody({
+          userId: "64eb651431a0030ecfa1cfb1",
+          title: "",
+          content: "",
+          image: [],
+          location: "India",
+        });
+      }
     } catch (e) {
       console.log(e);
     }
@@ -21,6 +49,113 @@ function Home() {
 
   return (
     <div className="Home_Container">
+      {/* form component */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <input
+          placeholder="Enter Title"
+          value={body.title}
+          onChange={(e) =>
+            setBody({
+              ...body,
+              title: e.target.value,
+            })
+          }
+        />
+
+        <textarea
+          placeholder="Enter Content"
+          value={body.content}
+          onChange={(e) => {
+            setBody({
+              ...body,
+              content: e.target.value,
+            });
+          }}
+        />
+
+        <label
+          style={{
+            marginBottom: "1.5rem",
+          }}
+        >
+          Select type of image
+          <select
+            value={activeOption}
+            onChange={(e) => setActiveOption(e.target.value)}
+            style={{
+              width: "120px",
+            }}
+          >
+            {[
+              {
+                title: "Link",
+                value: "link",
+              },
+              {
+                title: "File",
+                value: "file",
+              },
+            ].map((item, index) => (
+              <option value={item.value}>{item.title}</option>
+            ))}
+          </select>
+        </label>
+
+        {activeOption === "link" && (
+          <div>
+            <input
+              placeholder="Add image URL"
+              value={imgUrl}
+              onChange={(e) => setImgUrl(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                if (imgUrl.trim().length === 0) {
+                  return alert("Add valud image url");
+                }
+                setBody({
+                  ...body,
+                  image: [...body.image, imgUrl],
+                });
+                setImgUrl("");
+              }}
+            >
+              Add
+            </button>
+            {body.image.map((item, index) => (
+              <>
+                <img
+                  src={item}
+                  alt="item"
+                  style={{
+                    width: "120px",
+                    marginRight: "1rem",
+                  }}
+                />
+                <button
+                  onClick={() =>
+                    setBody({
+                      ...body,
+                      image: body.image.filter((img, idx) => img !== item),
+                    })
+                  }
+                >
+                  DEL
+                </button>
+              </>
+            ))}
+          </div>
+        )}
+
+        <button onClick={savePost}>SAVE</button>
+      </div>
+
+      {/* rendering a list of data */}
       {blogData.map((item, index) => (
         <div className="Blog_Container">
           <h4>{item?.title}</h4>
--- client/src/services/http.service.js
+++ client/src/services/http.service.js
@@ -5,9 +5,15 @@ const BASE_URL = "http://localhost:4000";
 
 const BLOG_URLs = {
   getAllBlogs: `/blogs`,
+  saveBlog: `/blogs/new-blog`,
 };
 
 export const getAllBlogs = async () => {
   const { data } = await http.get(BLOG_URLs.getAllBlogs);
+  return data;
+};
+
+export const saveBlog = async (body) => {
+  const { data } = await http.post(BLOG_URLs.saveBlog, body);
   return data;
 };
