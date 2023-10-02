40f6fafc5e13aa6433bbfacb7f6f54c84ed5ac15
Lavesh Gaurav <39463503+laveshgaurav@users.noreply.github.com>
2023-10-02 20:03:48 +0530

    Update AboutUs.js

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function AboutUs() {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (pos) => {
+        console.log(pos?.coords);
+        let { data } = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${pos?.coords?.latitude}&lon=${pos?.coords?.longitude}&appid=32b9e87afc1cadd4cc8b5cd0c4994fea`
+        );
+        console.log(data);
+      });
+    } else {
+      console.log("Location off");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return <div>AboutUs</div>;
 }
 
